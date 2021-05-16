f06f982f226b3efe4129b54022fd01a14262689d
D. Yu <dyu@fastmail.com>
2021-05-16 20:01:08 +0800

    Add dict of VUL fund codes and names.

--- api_client.py
+++ api_client.py
@@ -5,8 +5,37 @@ import requests
 import sys
 
 
-# Fund codes: fund names.
-# SLPGF:    Sun Life Phils - Peso Growth Fund
+
+# VULs - https://www.sunlife.com.ph/en/insurance/vul-fund-prices/
+VUL_FUND_CODES = {
+    "SLPBA": "Sun Life Phils - Peso Balanced Fund",
+    "SLPBF": "Sun Life Phils - Peso Bond Fund",
+    "SLPCP": "Sun Life Phils - Captains Fund",
+    "SLPDF": "Dynamic Fund",
+    "SLPEF": "Sun Life Phils - Peso Equity Fund",
+    "SLPGF": "Sun Life Phils - Peso Growth Fund",
+    "SLPGP": "Sun Life Phils - Growth Plus Fund",
+    "SLPIF": "Sun Life Phils - Peso Income Fund",
+    "SLPIN": "Sun Life Phils - Index Fund",
+    "SLPMM": "Sun Life Phils - Money Market Fund",
+    "SLPOF": "Sun Life Phils - Peso Opportunity Fund",
+    "SLPOT": "Sun Life Phils - Opportunity Tracker Fund",
+    "SLPP1": "Sun Peso Maximizer - Fund",
+    "SLPP2": "Sun Peso Maximizer - Primo 2 Fund",
+    "TDF20": "Sun Life Phils - Peso MyFuture 2020",
+    "TDF25": "Sun Life Phils - Peso MyFuture 2025",
+    "TDF30": "Sun Life Phils - Peso MyFuture 2030",
+    "TDF35": "Sun Life Phils - Peso MyFuture 2035",
+    "TDF40": "Sun Life Phils - Peso MyFuture 2040",
+    "SLUBF": "Sun Life Phils - Dollar Bond Fund",
+    "SLUD7": "Sun Life Phils - Sun Dollar Maximizer - WT",
+    "SLUD8": "Sun Life Phils - Sun Dollar Maximizer - PriMO",
+    "SLUD9": "Sun Life Phils - Sun Dollar Maximizer - PriMO 2",
+    "SLUGF": "Sun Life Phils - Global Growth Fund",
+    "SLUIF": "Sun Life Phils - Global Income Fund",
+    "SLUOF": "Sun Life Phils - Global Opportunity Fund",
+    "SLUMM": "Sun Life Phils - Dollar Money Market Fund",
+}
 
 def call_api(fund_code, start_date, end_date):
     url = "https://www.sunlife.com.ph/funds/navprice/vul?version=1&language=en-us"
